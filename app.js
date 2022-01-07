cf5a4a0ec59852d03ab5fc1ee4d91ea7a4bfda08
Jamie Storey <jamie@jamiestorey.email>
2022-01-07 23:51:38 +0000

    allow put requests with methodOverride

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotnev = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 //const MongoStore = require('connect-mongo')(session);
@@ -25,6 +26,18 @@ const app = express();
 // BODY PARSER
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+      }
+    })
+  );
 
 // LOGGING
 if (process.env.NODE_ENV === 'development') {

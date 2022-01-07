5e8eba83730b991ef380083fd7f7cfa4c20c2875
Jamie Storey <jamie@jamiestorey.email>
2022-01-07 23:20:47 +0000

    added globabl variable for user

--- app.js
+++ app.js
@@ -69,6 +69,12 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+  });
+
 // STATIC CONTENT FOLDER
 app.use(express.static(path.join(__dirname, 'public')));
 

bce9a9ff7fe2cc8c3cd4152aed6fa943467dec35
Jamie Storey <jamie@jamiestorey.email>
2022-01-26 22:28:30 +0000

    Added model for betting on a fixture

---
+++ models/BetFixture.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const BetFixtureSchema = new mongoose.Schema({
+    fixtureID: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    guessHomeScore: {
+        type: Number,
+        required: true,
+        default: -2
+    },
+    guessAwayScore: {
+        type: Number,
+        required: true,
+        default: -4
+    },
+    pointsEarned: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    status: {
+        type: String,
+        default: 'pending',
+        enum: ['pending', 'won', 'lost', 'draw']
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('BetFixture', BetFixtureSchema);

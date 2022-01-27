058c6d0c00b5d3cf33055aab84f0833d30d0083c
Jamie Storey <jamie@jamiestorey.email>
2022-01-27 23:52:42 +0000

    Added fixture model - needs improving

---
+++ models/Fixture.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const FixtureSchema = new mongoose.Schema({
+    fixture_id: {
+        type: String,
+        required: true
+    },
+    fixture_date: {
+        type: String,
+        required: true,
+    },
+    "fixture_time": {
+        type: String
+    },
+    "fixture_status": {
+        type: String
+    },
+    "fixture_league_id": {
+        type: String
+    },
+    "fixtures_round": {
+        type: String
+    },
+    "fixture_team_home_id": {
+        type: String
+    },
+    "fixture_team_away_id": {
+        type: String
+    },
+    "fixture_team_home_name": {
+        type: String
+    },
+    "fixture_team_away_name": {
+        type: String
+    },
+    "fixture_team_home_is_winner": {
+        type: String
+    },
+    "fixture_team_away_is_winner": {
+        type: String
+    },
+    "fixture_goals_home": {
+        type: String
+    },
+    "fixture_goals_away": {
+        type: String
+    },
+});
+
+module.exports = mongoose.model('Fixture', FixtureSchema);

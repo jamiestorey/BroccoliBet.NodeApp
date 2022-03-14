e9e6dfac90d8afb0ead416904081a3638a569163
Jamie Storey <jamie@jamiestorey.email>
2022-03-14 23:28:20 +0000

    Add a todo comment for editing

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -17,6 +17,7 @@ module.exports = {
   stripTags: function (input) {
     return input.replace(/<(?:.|\n)*?>/gm, '')
   },
+  // TODO: use and edit icon for bets?
   editIcon: function (storyUser, loggedUser, storyId, floating = true) {
     if (storyUser._id.toString() == loggedUser._id.toString()) {
       if (floating) {
